11f9d2bd548639e891e33951a3fb335b7a38b18e
Atharva Mahadik <atharvamahadik08@gmail.com>
2025-10-27 15:45:26 -0400

    changes

--- app/Labs/Lab4/ReduxExamples.tsx/page.tsx
+++ app/Labs/Lab4/ReduxExamples.tsx/page.tsx
@@ -1,13 +1,12 @@
 import AddRedux from "./AddRedux/page";
 import CounterRedux from "./CounterRedux/page";
-import HelloRedux from "./HelloRedux/page";
 import TodoList from "./todos/TodoList";
 
 export default function ReduxExamples() {
     return(
         <div>
             <h2>Redux Examples</h2>
-            <HelloRedux/>
+            {/* <HelloRedux/> */}
             <CounterRedux/>
             <AddRedux/>
             <TodoList/>
--- app/Labs/Lab4/store/index.tsx
+++ app/Labs/Lab4/store/index.tsx
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import helloReducer from "../ReduxExamples.tsx/HelloRedux/helloReducer";
 import counterReducer from "../ReduxExamples.tsx/CounterRedux/counterReducer";
 import addReducer from "../ReduxExamples.tsx/AddRedux/addReducer";
 import todosReducer from "../ReduxExamples.tsx/todos/todosReducer";
 
 const store = configureStore({
-    reducer : {helloReducer,
+    reducer : {
         counterReducer,
         addReducer,
         todosReducer,
