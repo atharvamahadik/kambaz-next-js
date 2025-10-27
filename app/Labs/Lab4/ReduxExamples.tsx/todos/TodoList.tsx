dbd972c5a8692cf196b367b020c2448583cfc467
Atharva Mahadik <atharvamahadik08@gmail.com>
2025-10-27 13:39:38 -0400

    Build failure

--- app/Labs/Lab4/ReduxExamples.tsx/todos/TodoList.tsx
+++ app/Labs/Lab4/ReduxExamples.tsx/todos/TodoList.tsx
@@ -4,14 +4,27 @@ import { Button, FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
+
+interface Todo {
+  id: string;
+  title: string;
+}
+
+interface RootState {
+  todosReducer: {
+    todos: Todo[];
+    todo: Todo | { title: string };
+  };
+}
+
 export default function TodoList() {
-  const { todos } = useSelector((state: any) => state.todosReducer);
+  const { todos } = useSelector((state: RootState) => state.todosReducer);
   return (
     <div>
       <h2>Todo List</h2>
       <ListGroup>
         <TodoForm/>
-        {todos.map((todo: any) => (
+        {todos.map((todo: Todo) => (
           <TodoItem 
           todo={todo}
           key={todo.id} />
