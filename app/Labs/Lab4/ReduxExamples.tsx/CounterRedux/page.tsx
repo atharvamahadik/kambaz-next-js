661f88190fa033bacd5da06f84d9df9ae2aaa38d
Atharva Mahadik <atharvamahadik08@gmail.com>
2025-10-27 15:37:45 -0400

    changes

--- app/Labs/Lab4/ReduxExamples.tsx/CounterRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples.tsx/CounterRedux/page.tsx
@@ -1,26 +1,65 @@
 "use client";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterReducer";
 
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { Provider, useSelector, useDispatch } from "react-redux";
 interface CounterState {
   count: number;
 }
 
-interface RootState {
-  counterReducer: CounterState; 
-}
+const initialState: CounterState = {
+  count: 0,
+};
 
-export default function CounterRedux() {
-  const { count } = useSelector((state: RootState) => state.counterReducer);
-  const dispatch = useDispatch();
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+const { increment, decrement } = counterSlice.actions;
+const store = configureStore({
+  reducer: {
+    counterReducer: counterSlice.reducer,
+  },
+});
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+function CounterReduxInner() {
+  const count = useSelector((state: RootState) => state.counterReducer.count);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <div id="wd-counter-redux">
       <h2>Counter Redux</h2>
       <h3>{count}</h3>
-      <button onClick={() => dispatch(increment())}
-              id="wd-counter-redux-increment-click"> Increment </button>
-      <button onClick={() => dispatch(decrement())}
-              id="wd-counter-redux-decrement-click"> Decrement </button>
-      <hr/>
+      <button
+        id="wd-counter-redux-increment-click"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <button
+        id="wd-counter-redux-decrement-click"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      <hr />
     </div>
-);}
+  );
+}
+export default function CounterRedux() {
+  return (
+    <Provider store={store}>
+      <CounterReduxInner />
+    </Provider>
+  );
+}
