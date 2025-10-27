709ec214f5570553fc5b85918afd59853963217a
Atharva Mahadik <atharvamahadik08@gmail.com>
2025-10-27 15:03:21 -0400

    build failure

--- app/Labs/Lab4/EventObject.tsx
+++ app/Labs/Lab4/EventObject.tsx
@@ -1,22 +1,43 @@
 import { useState } from "react";
+import React from "react";
 
+interface SimpleEventData {
+  type: string;
+  timeStamp: number;
+  button: number;
+  clientX: number;
+  clientY: number;
+  targetHTML: string;
+}
 
 export default function EventObject() {
-  const [event, setEvent] = useState(null);
-  const handleClick = (e: any) => {
-    e.target = e.target.outerHTML;
-    delete e.view;
-    setEvent(e);
+  
+  const [eventData, setEventData] = useState<SimpleEventData | null>(null);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const simpleEventData: SimpleEventData = {
+      type: e.type,
+      timeStamp: e.timeStamp,
+      button: e.button,
+      clientX: e.clientX,
+      clientY: e.clientY,
+      targetHTML: (e.target as HTMLButtonElement).outerHTML,
+    };
+    setEventData(simpleEventData);
   };
+
   return (
     <div>
       <h2>Event Object</h2>
-      <button onClick={(e) => handleClick(e)}
+      <button
+        onClick={handleClick}
         className="btn btn-primary"
-        id="wd-display-event-obj-click">
+        id="wd-display-event-obj-click"
+      >
         Display Event Object
       </button>
-      <pre>{JSON.stringify(event, null, 2)}</pre>
-      <hr/>
+      <pre>{JSON.stringify(eventData, null, 2)}</pre>
+      <hr />
     </div>
-);}
+  );
+}
